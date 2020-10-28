28517d12d5c14dfa20ba207d85a0bfcae1c971a0
yluego <vcrod79@gmail.com>
2020-10-27 20:02:15 -0700

    added-json

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,6 +1,19 @@
-var getUserRepos = function () {
-  fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
 };
+
+console.log("outside");
+//console.log(response);
+var response = fetch("https://api.github.com/users/octocat/repos");
+console.log(response);
+
 
 getUserRepos();
